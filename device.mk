a3d8836ba19de4c624ad64367c5aa20702c2c326
AstralNovah-Brazilian <prodbylivehorde@gmail.com>
2026-02-05 04:44:36 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -13,19 +13,10 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# Boot control HAL
-# PRODUCT_PACKAGES += \
-#    android.hardware.boot@1.0-impl \
-#    android.hardware.boot@1.0-service
-
 PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl.recovery \
     bootctrl.mt6879
 
-# PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-#    bootctrl.mt6879 \
-#    libgptutils \
-#    libz \
-#    libcutils
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
